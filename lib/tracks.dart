c3b246bddef0b898313bc336bb4c15d78afd5e0d
sylvain.villet <G4mm4R4yyy$>
2020-07-16 16:18:16 +0200

    Added ST lap times

--- lib/tracks.dart
+++ lib/tracks.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Track {
   const Track(this.ksName, this.displayName, this.length, this.lapTimeGT3,
-      this.lapTimeGT4, this.lapTimeCUP, this.timeLostInPits);
+      this.lapTimeGT4, this.lapTimeCUP, this.lapTimeST, this.timeLostInPits);
 
   final String ksName;
   final String displayName;
@@ -10,6 +10,7 @@ class Track {
   final double lapTimeGT3;
   final double lapTimeGT4;
   final double lapTimeCUP;
+  final double lapTimeST;
   final int timeLostInPits;
 
   double getLapTime(String className)
@@ -21,7 +22,7 @@ class Track {
     else if (className == 'CUP')
       return lapTimeCUP;
     else if (className == 'ST')
-      return lapTimeGT3 * 1.03;
+      return lapTimeST;
     else
       return 0.0;
   }
@@ -38,19 +39,19 @@ Track getTrack(String ksName)
 }
 
 const List<Track> tracksList = [
-  Track("monza",          "Monza",          5793, 106.7, 117.4, 109.9, 60),
-  Track("Brands_Hatch",   "Brands Hatch",   3908, 83.9,  90.7,  85.6,  60),
-  Track("Silverstone",    "Silverstone",    5891, 117.8, 130.2, 122.4, 60),
-  Track("Paul_Ricard",    "Paul Ricard",    5842, 114.2, 124.0, 116.6, 60),
-  Track("misano",         "Misano",         4226, 93.8,  102.3, 96.6,  60),
-  Track("zandvoort",      "Zandvoort",      4252, 95.0,  104.8, 98.4,  60),
-  Track("Spa",            "Spa",            7004, 138.0, 151.4, 142.7, 90),
-  Track("nurburgring",    "Nürburgring",    5137, 114.1, 125.4, 117.7, 60),
-  Track("hungaroring",    "Hungaroring",    4381, 104.5, 113.3, 106.8, 60),
-  Track("barcelona",      "Barcelona",      4665, 104.1, 114.2, 107.7, 60),
-  Track("zolder",         "Zolder",         4011, 87.8,  95.1,  91.8,  60),
-  Track("mont_panorama",  "Mount Panorama", 6213, 120.6, 133.5, 125.3, 60),
-  Track("Laguna_Seca",    "Laguna Seca",    3602, 82.6,  90.0,  84.9,  60),
-  Track("Suzuka",         "Suzuka",         5807, 119.7, 131.2, 123.5, 60),
-  Track("Kyalami",        "Kyalami",        4522, 101.0, 110.4, 104.3, 60),
+  Track("barcelona",      "Barcelona",      4665, 104.1, 114.2, 107.7, 108.6, 60),
+  Track("Brands_Hatch",   "Brands Hatch",   3908, 83.9,  90.7,  85.6,  86.5,  60),
+  Track("hungaroring",    "Hungaroring",    4381, 104.5, 113.3, 106.8, 108.2, 60),
+  Track("Kyalami",        "Kyalami",        4522, 101.0, 110.4, 104.3, 105.5, 60),
+  Track("Laguna_Seca",    "Laguna Seca",    3602, 82.6,  90.0,  84.9,  85.5,  60),
+  Track("misano",         "Misano",         4226, 93.8,  102.3, 96.6,  97.6,  60),
+  Track("mont_panorama",  "Mount Panorama", 6213, 120.6, 133.5, 125.3, 127.9, 60),
+  Track("monza",          "Monza",          5793, 106.7, 117.4, 109.9, 111.9, 60),
+  Track("nurburgring",    "Nürburgring",    5137, 114.1, 125.4, 117.7, 119.3, 60),
+  Track("Paul_Ricard",    "Paul Ricard",    5842, 114.2, 124.0, 116.6, 119.0, 60),
+  Track("Silverstone",    "Silverstone",    5891, 117.8, 130.2, 122.4, 122.8, 60),
+  Track("Spa",            "Spa",            7004, 138.0, 151.4, 142.7, 145.2, 90),
+  Track("Suzuka",         "Suzuka",         5807, 119.7, 131.2, 123.5, 125.6, 60),
+  Track("zandvoort",      "Zandvoort",      4252, 95.0,  104.8, 98.4,  99.6,  60),
+  Track("zolder",         "Zolder",         4011, 87.8,  95.1,  91.8,  92.8,  60),
 ];
